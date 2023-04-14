8072ab483ede7f576f92146ea0b8064eedc8e736
Taize Moreira <taizebueno@uniplaclages.edu.br>
2023-04-14 16:08:43 -0300

    Master of Code

---
+++ Exercicio02/LerCotacaoDolar.cs
@@ -0,0 +1,67 @@
+﻿////Faça um programa que:
+//-Leia a cotação do dólar
+//- Leia um valor em dólares
+//- Converta esse valor para Real
+//- Mostre o resultado
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio02
+{
+    public class LerCotacaoDolar
+    {
+        static void Main(string[] args)
+        {
+            // personaliza o cabeçalho do console
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Clear();
+
+            Console.WriteLine("**********************************");
+            Console.WriteLine("****    COTAÇÃO DO DÓLAR    ******");
+            Console.WriteLine("**********************************");
+            Console.WriteLine();
+
+            // Leia a cotação do dólar
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("Informe a cotação do dolar: ");
+            double cotacaoDolar = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor em dólares:");
+            double valorEmDolar = double.Parse(Console.ReadLine());
+
+            // personaliza o console com sons e cores
+            Console.Beep(2000, 500);
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine();
+            Console.WriteLine("Realizando o cálculo...");
+            Console.WriteLine();
+
+            // Converta esse valor para Real
+            double valorEmReais = (valorEmDolar * cotacaoDolar);
+
+            //Mostre o resultado
+            Console.WriteLine("**********************************");
+            Console.WriteLine("**** RESULTADO DO CÁLCULO ********");
+            Console.WriteLine("**********************************");
+            Console.WriteLine();
+
+            Console.WriteLine("O valor em reais é: " + valorEmReais);
+           
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("Pressione qualquer tecla para encerrar o programa...");
+
+            // espera que o usuário pressione uma tecla para encerrar o programa
+            Console.ReadKey();
+        }
+    }
+}
--- Exercicio03/Program.cs
+++ Exercicio03/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//// 3) Faça um programa para pagamento de comissão de vendedores de peças, levando-se em consideração que sua
+//comissão será de 5% do total da venda e que você tem os seguintes dados:
+//-Identificação do vendedor
+//- Código da peça
+//- Preço unitário da peça
+//- Quantidade vendida 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio02
+{
+    public class LerCotacaoDolar
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Clear();
+
+            Console.WriteLine("**********************************");
+            Console.WriteLine("****  PAGAMENTO DE COMISSÃO   ****");
+            Console.WriteLine("**********************************");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("Informe a identificação do vendedor: ");
+            string nomeVendedor = Console.ReadLine();
+
+            Console.WriteLine("Informe o código da peça:");
+            double codigoPeca = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe o preço unitário da peça:");
+            double precoUnitarioPeca = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe a quantidade vendida:");
+            double qtdeVendida = Convert.ToDouble(Console.ReadLine());
+
+            // personaliza o console com sons e cores
+            Console.Beep(2000, 500);
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine();
+            Console.WriteLine("Realizando o cálculo...");
+            Console.WriteLine();
+
+            // Calculando a comissão
+            double totalVenda = precoUnitarioPeca * qtdeVendida;
+
+            double comissaoVendedor = (totalVenda * 0.05);
+
+            //Mostre o resultado
+            Console.WriteLine("1. Identificação do vendedor: " + nomeVendedor);
+            Console.WriteLine("2. Código da peça: " + codigoPeca);
+            Console.WriteLine("3. Preço unitário da peça: " + precoUnitarioPeca);
+            Console.WriteLine("4. Quantidade de peças vendidas: " + qtdeVendida);
+            Console.WriteLine("5. O valor total da venda será de R$: " + totalVenda + " reais");
+
+            Console.WriteLine();
+
+            Console.WriteLine("COMISSÃO DO VENDEDOR: " + comissaoVendedor + " reais");
+
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("Pressione qualquer tecla para encerrar o programa...");
+
+            // espera que o usuário pressione uma tecla para encerrar o programa
+            Console.ReadKey();
+        }
+    }
+}
